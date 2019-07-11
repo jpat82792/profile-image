c5487fa7294ae7e2532c1f1ab9ce996722568643
Jonathan Patterson <jpat82792@gmail.com>
2019-07-10 23:28:26 -0400

    Add logging to verify variable settings

--- startDeveloping.sh
+++ startDeveloping.sh
@@ -1,4 +1,10 @@
 set projectDirectory=pwd
 echo "Watching Sass files for changes"
 x-terminal-emulator -e gulp watchSass &
-x-terminal-emulator -e docker run -v $(pwd):/usr/src/app profile-image gulp startServer
+export sassWatcherPid=$!
+x-terminal-emulator -e docker run -v $(pwd):/usr/src/app profile-image gulp startServer &
+export dockerPid=$!
+echo 'sassWatcherPid'
+echo $sassWatcherPid
+echo 'dockerPid'
+echo $dockerPid
