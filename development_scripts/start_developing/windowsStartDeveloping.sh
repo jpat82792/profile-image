761565ffed2b961cb04c63daccf25b75805de281
Jonathan Patterson <jpat82792@gmail.com>
2019-09-07 08:25:17 -0400

    Remove trailing cmd /k

--- development_scripts/start_developing/windowsStartDeveloping.sh
+++ development_scripts/start_developing/windowsStartDeveloping.sh
@@ -1,11 +1,13 @@
 cd ../..
 set projectDirectory=pwd
 echo "Spawning off child processes"
-echo "Start building docker image"
-# start docker-compose build
+echo "Start javascript unit tests"
+start "Javascript unit testing" npm run test 
 echo "Begin Watching Sass files for changes"
 start "Watching Sass files" gulp watchSass
+echo "Start building docker image"
+# start docker-compose build
 echo "Start the docker node container"
-start "Node server" docker-compose up && cmd /k
+start "Node server" docker-compose up
 echo "Done spawning child processes" 
 cmd /k
